7572ca0d34e12b6263e5a0f0d930e35c76d12d98
Igor Shklyar <igor.shklyar@gmail.com>
2018-02-15 16:15:59 +0200

    275 Setup Project Routes

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="container">
-
+      <div class="row">
+          <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+              <app-header></app-header>
+              <hr>
+              <router-view></router-view>
+          </div>
+      </div>
   </div>
 </template>
 
 <script>
+import Header from './components/Header.vue';
 
-  export default {
-
-  }
+export default {
+    components: {
+        appHeader: Header
+    }
+}
 </script>
 
 <style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,15 @@
 <template>
-    <h1>The Header Component</h1>
+    <div>
+        <ul class="nav nav-pills">
+            <li role="presentation">
+                <router-link to="/">Home</router-link>
+            </li>
+            <li role="presentation">
+                <router-link to="/portfolio">Portfolio</router-link>
+            </li>
+            <li role="presentation">
+                <router-link to="/stocks">Stocks</router-link>
+            </li>
+        </ul>
+    </div>
 </template>
